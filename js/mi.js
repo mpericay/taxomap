12c258b29e588e3af2f0767ba788f1b3d2e8b7cf
Marti Pericay <marti.pericay@gmail.com>
2014-06-13 18:26:23 +0200

    IE9 bug

--- js/mi.js
+++ js/mi.js
@@ -176,7 +176,7 @@ var MI = {
         var bounds = this.getBoundsFromPosition(position);
         var childrenLevel = (UI.taxon.levels[UI.taxon.level+1]) ? UI.taxon.level + 1 : UI.taxon.level;
         
-        $.getJSON(this.cartodbApi,
+        $.getJSON(this.cartodbApi + "callback=?",
             {
             q: "select count(*)," + UI.taxon.levelsId[childrenLevel] + " as id," + UI.taxon.levels[childrenLevel] + " as name from " + this.cartodbTable + " where " + UI.taxon.levelsId[UI.taxon.level] +"='" + UI.taxon.id + "' and (the_geom && ST_SetSRID(ST_MakeBox2D(ST_Point("+bounds.left+","+bounds.bottom+"),ST_Point("+bounds.right+","+bounds.top+")),4326)) group by id, name order by count(*) desc"
             //BBOX: bounds.toBBOX(3,false)
