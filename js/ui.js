6dbf96228b96b15a2bbebb0fe65c424d6588f27f
Marti Pericay <marti.pericay@gmail.com>
2015-07-13 19:27:12 +0200

    Close #5

--- js/ui.js
+++ js/ui.js
@@ -438,18 +438,23 @@ var UI = {
 				text: false,
 				icons: {
 				primary: "ui-icon-triangle-1-s"
-				}
+				} 
 				})
 				.click(function() {
-					var menu = $( this ).parent().next().show().position({
-					my: "left top",
-					at: "left bottom",
-					of: this
-				});
-				$( document ).one( "click", function() {
-					menu.hide();
-				});
-				return false;
+					var menu = $( this ).parent().next();
+					if( menu.is(':visible') ) {
+						menu.hide();
+						return false; 
+					} 
+					menu.show().position({
+						my: "left top",
+						at: "left bottom",
+						of: this
+					});
+					$( document ).one( "click", function() {
+						menu.hide();
+					});
+					return false;
 				})
 				.parent()
 				.buttonset()
